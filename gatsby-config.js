2dee7977afb06bc83731f8633d30304e90fa41c2
Stephen Woods <stephen.woods@altarasystems.com>
2019-05-27 18:00:19 -0400

    Added  plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    `gatsby-plugin-netlify-cms`
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify`
   ],
 }
