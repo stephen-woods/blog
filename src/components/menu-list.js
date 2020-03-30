fe2454e4ccffb864db13602cc824c731c9699014
Stephen Woods <stephen.woods@altarasystems.com>
2020-03-30 11:29:41 -0400

    Removed about link temporarily

--- src/components/menu-list.js
+++ src/components/menu-list.js
@@ -22,8 +22,7 @@ export default () => {
 
   return <nav className={s.nav}>
     <Link to="/" className={s.nav_a}>Home</Link>
-    <Link to="/about" className={s.nav_a}>About</Link>
-    <Link to="/resume" className={s.nav_a}>Resume</Link>
     <Link to="/blog" className={s.nav_al}>Blog</Link>
+    <Link to="/resume" className={s.nav_a}>Resume</Link>
   </nav>
 }
