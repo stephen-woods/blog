d9d00b13ebfcbc1ae331b961912a8e90ca967cde
Stephen Woods <stephen_woods@cable.comcast.com>
2023-02-10 12:39:17 -0500

    Added feature image to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,22 +1,31 @@
-import React from "react"
+import * as React from "react"
 import {Link, graphql} from "gatsby"
+import {GatsbyImage, getImage} from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import  * as styles from "./blog-post.module.css"
+
+const heroImageStyle = {
+    height: "300px"
+}
+
+const bodyStyle = {
+    margin: "0 0 0 0"
+}
 
 const BlogPost = (props) => {
     const post = props.data.mdx
     const siteTitle = props.data.site.siteMetadata.title
     const {previous, next} = props.pageContext
-    const featuredImgFluid = post.frontmatter.featuredImage.childImageSharp.fluid
+    const featuredImage = getImage(post.frontmatter.featuredImage)
     return (
         <Layout location={props.location} title={siteTitle}>
-
             <div className={styles.blogbanner}>
+                <GatsbyImage image={featuredImage} alt="" style={heroImageStyle}/>
                 <span className={styles.title}>{post.frontmatter.title}</span>
                 <span className={styles.subtitle}>{post.frontmatter.date}</span>
             </div>
             <div className={styles.blogbody}>
-               {props.children}
+                {props.children}
 
                 <hr/>
 
@@ -60,21 +69,30 @@ export const pageQuery = graphql`
       }
     }
     mdx(fields: { slug: { eq: $slug } }) {
-      id
-      excerpt(pruneLength: 160)
-    
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
         titleClass
         featuredImage {
           childImageSharp {
-            fluid(maxWidth: 800) {
-              ...GatsbyImageSharpFluid
-            }
+            gatsbyImageData(
+              layout: FULL_WIDTH
+            )
           }
         }
       }
     }
   }
 `
+
+export const Head = (props) => {
+    const siteTitle = props.data.site.siteMetadata.title
+    const title = props.data.mdx.frontmatter.title
+    return (
+        <>
+            <title>{siteTitle} - {title}</title>
+            <meta name="description" content="Hello World"/>
+            <body style={bodyStyle}/>
+        </>
+    )
+}
